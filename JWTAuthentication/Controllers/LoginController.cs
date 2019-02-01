86fb4c6fd9c6da9d05fe8c121788199e3609b90d
Tushti02 <ahuja.tushti@gmail.com>
2019-02-01 16:11:46 +0530

    initial Commit

---
+++ JWTAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWTAuthentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody]UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if(user!=null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+            }
+            else
+            {
+                return BadRequest("Invalid client request");
+            }
+            return View();
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(issuer:_config["Jwt:Issuer"],
+              audience:_config["Jwt:Issuer"],
+              claims:null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            //Validate the User Credentials  
+            //Demo Purpose, I have Passed HardCoded User Information  
+            if (login.Username == "Jignesh")
+            {
+                user = new UserModel { Username = "Jignesh Trivedi", EmailAddress = "test.btest@gmail.com" };
+            }
+            return user;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+    }
+}
